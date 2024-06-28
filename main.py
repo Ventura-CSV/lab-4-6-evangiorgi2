7a6a8feb1c983902dd45a4ae32797aeb6f6d0b67
Evan Giorgi <evan_giorgi@verizon.net>
2024-06-27 18:47:49 -0700

    Not correct print

--- main.py
+++ main.py
@@ -2,14 +2,11 @@ def main():
     iternum = 0
     N = int(input('Enter the value for N: '))
     for i in range(N):
-        for j in """ COMPLETE FOR LOOP STATEMENT """
+        for j in range(0, N):
             print(f'({i}, {j})', end=' ')
             iternum += 1
         print()
 
-    ########################################
-    # Do not delete the return statement
-    ########################################
     return iternum
 
 
